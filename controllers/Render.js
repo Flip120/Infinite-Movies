c7e4aa79a998038e897eb2f22cafe90d3c5953d1
hooptap-ccabanero <c.cabanerochaparro@gmail.com>
2015-10-02 01:40:08 +0200

    Added the possibility to render the app index from server with jade

--- controllers/Render.js
+++ controllers/Render.js
@@ -1,7 +1,7 @@
 var ClientController = function(){};
 
 ClientController.prototype.renderIndex = function(req, res, next){
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Infinite Movies' });
 };
 
 module.exports = ClientController;
